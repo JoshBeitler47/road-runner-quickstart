900febfa232c60bb8bff068e1f353e1562d22a52
JoshBeitler47 <beitlerjosh@gmail.com>
2024-05-02 16:18:21 -0400

    Fixed CD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainCode/CommunityDay/CDMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainCode/CommunityDay/CDMain.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.MainCode.CommunityDay;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,8 +16,6 @@ public final class CDMain extends LinearOpMode {
     public MecanumDrive drive;
     public Pose2d currentPose;
     public int sq = 12;
-    public int neg = 1;
-    public double currentHeading = 90;
 
     public void runOpMode() throws InterruptedException {
         double xStart = 60;
@@ -31,116 +30,41 @@ public final class CDMain extends LinearOpMode {
 
 ///////
         //Type Your Code Below!
-        //driveForward(1);
-        //driveBackward(2);
-        //turnRight();
-        //turnLeft();
-
-        driveForward(2);
-        turnRight();
-        turnLeft();
-        driveForward(2);
-
+        Forward(2);
+        Right(1);
+        Backward(1);
+        Forward(2);
+        Left(1);
 
 //////
     }
 
-    public void driveForward(int amnt) {
-        if ((currentHeading == 90) || (currentHeading == 270)) {
-            if (currentHeading == 270) {
-                neg = -1;
-            }
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .lineToYConstantHeading(currentPose.position.y + (sq * amnt * neg))
-                            .build());
-            neg = 1;
-        } else if ((currentHeading == 180) || (currentHeading == 0)) {
-            if (currentHeading == 180) {
-                neg = -1;
-            }
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .lineToXConstantHeading(currentPose.position.x + (sq * amnt * neg))
-                            .build());
-            neg = 1;
-        }
-    }
-    public void driveBackward(int amnt) {
-        if ((currentHeading == 90) || (currentHeading == 270)) {
-            if (currentHeading == 90) {
-                neg = -1;
-            }
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .lineToYConstantHeading(currentPose.position.y + (sq * amnt * neg))
-                            .build());
-            neg = 1;
-        } else if ((currentHeading == 180) || (currentHeading == 0)) {
-            if (currentHeading == 0) {
-                neg = -1;
-            }
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .lineToXConstantHeading(currentPose.position.x + (sq * amnt * neg))
-                            .build());
-            neg = 1;
-        }
-    }
-    public void turnRight() {
-        if (currentHeading == 0){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(270))
-                            .build());
-            currentHeading = 270;
-        } else if (currentHeading == 90){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(0))
-                            .build());
-            currentHeading = 0;
-        } else if (currentHeading == 180){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(90))
-                            .build());
-            currentHeading = 90;
-        } else if (currentHeading == 270){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(180))
-                            .build());
-            currentHeading = 180;
-        }
+    public void Forward(int amnt) {
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .lineToYConstantHeading(drive.pose.position.y + (sq * amnt))
 
+                        .build());
     }
-    public void turnLeft(){
-        if (currentHeading == 0){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(90))
-                            .build());
-            currentHeading = 90;
-        } else if (currentHeading == 90){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(180))
-                            .build());
-            currentHeading = 180;
-        } else if (currentHeading == 180){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(270))
-                            .build());
-            currentHeading = 270;
-        } else if (currentHeading == 270){
-            Actions.runBlocking(
-                    drive.actionBuilder(drive.pose)
-                            .turnTo(Math.toRadians(0))
-                            .build());
-            currentHeading = 0;
-        }
 
+    public void Backward(int amnt) {
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeToConstantHeading(new Vector2d(drive.pose.position.x, drive.pose.position.y - (sq * amnt)))
+                        .build());
+    }
+
+    public void Right(int amnt) {
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeToConstantHeading(new Vector2d(drive.pose.position.x + (sq * amnt), drive.pose.position.y))
+                        .build());
+    }
+
+    public void Left(int amnt) {
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeToConstantHeading(new Vector2d(drive.pose.position.x - (sq * amnt), drive.pose.position.y))
+                        .build());
     }
 }
